ddc47901ba03fdc92dce948eda01fbd6581017d7
BehruzToshmurodov <toshmurodovbehruz23@gmail.com>
2025-01-14 13:10:02 +0500

    Update Groups.java

--- src/main/java/uz/app/finalproject/entity/Groups.java
+++ src/main/java/uz/app/finalproject/entity/Groups.java
@@ -1,18 +1,11 @@
 package uz.app.finalproject.entity;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
