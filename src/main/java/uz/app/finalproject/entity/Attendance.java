70a5e00d127848efdd95d2562d84f219226fbd54
BehruzToshmurodov <toshmurodovbehruz23@gmail.com>
2025-01-14 13:10:04 +0500

    Update Attendance.java

--- src/main/java/uz/app/finalproject/entity/Attendance.java
+++ src/main/java/uz/app/finalproject/entity/Attendance.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
@@ -20,7 +21,7 @@ public class Attendance {
     @ManyToOne
     private Student student;
 
-    private LocalDateTime attendanceDate;
+    private LocalDate attendanceDate;
 
     private boolean attended;
 
