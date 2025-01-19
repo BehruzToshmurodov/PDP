640543f25ad9a59b79485f1767dd1ec8e747e46d
BehruzToshmurodov <toshmurodovbehruz23@gmail.com>
2025-01-19 14:32:52 +0500

    Finance deleted

--- src/main/java/uz/app/finalproject/repository/FinanceRepository.java
+++ src/main/java/uz/app/finalproject/repository/FinanceRepository.java
@@ -1,7 +1,6 @@
 package uz.app.finalproject.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import uz.app.finalproject.entity.Finance;
 
 import java.time.LocalDate;
 import java.util.List;
